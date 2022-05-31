b258eccf23f5cc2378250ec09dc0b8e276592138
denis <tv_bb@mail.ru>
2022-06-01 02:41:27 +0300

    добавлена кнопка очистить

--- js/Script.js
+++ js/Script.js
@@ -23,36 +23,38 @@ class html {
 
 
     addElement (data) {
-        let div = document.createElement ( 'div' );
-        div.className = 'tuduElement';
-        // div.style.border = 'solid'
-        div.style.margin = '20px';
-        this.classDiv.append ( div );
-        let span1 = document.createElement ( 'span' );
-        span1.innerText = data;
-        listener.valueInput.value = '';
-        div.append ( span1 );
-        let div2 = document.createElement ( 'div' );
-        div2.className = 'icons';
-        div.append ( div2 );
-        let span = document.createElement ( 'span' );
-        span.className = 'editSpan';
-        span.innerText = '✎';
-        span.onclick = (e) => this.editElement ( e );
-        div2.append ( span );
-        let span2 = document.createElement ( 'span' );
-        span2.className = 'deleteSpan';
-        span2.innerText = '🗑';
-        span2.onclick = (e) => e.target.parentNode.parentElement.remove ();
-        span2.style.margin = '20px';
-        div2.append ( span2 );
+        if(data) {
+            let div = document.createElement ( 'div' );
+            div.className = 'tuduElement';
+            // div.style.border = 'solid'
+            div.style.margin = '20px';
+            this.classDiv.append ( div );
+            let span1 = document.createElement ( 'span' );
+            span1.innerText = data;
+            listener.valueInput.value = '';
+            div.append ( span1 );
+            let div2 = document.createElement ( 'div' );
+            div2.className = 'icons';
+            div.append ( div2 );
+            let span = document.createElement ( 'span' );
+            span.className = 'editSpan';
+            span.innerText = '✎';
+            span.onclick = (e) => this.editElement ( e );
+            div2.append ( span );
+            let span2 = document.createElement ( 'span' );
+            span2.className = 'deleteSpan';
+            span2.innerText = '🗑';
+            span2.onclick = (e) => e.target.parentNode.parentElement.remove ();
+            span2.style.margin = '20px';
+            div2.append ( span2 );
+        }
     }
 
 
 };
 
 const html1 = new html ();
-html1.init(2)
+html1.init(0)
 html1.addElement()
 listener.postTodos()
 listener.getTodos()
