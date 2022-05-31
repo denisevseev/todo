f3746cf694b5183169584b3645066199abf32cc8
denis <tv_bb@mail.ru>
2022-05-31 19:28:48 +0300

    вынес листенеры в отдельный класс и отдельный фаил

---
+++ js/Requests.js
@@ -0,0 +1,47 @@
+import html1 from "./Script.js";
+class listeners {
+    constructor () {
+        this.save = document.querySelector ( '#save' );
+        this.addLi = document.querySelector ( '#addLi' );
+        this.clear = document.querySelector('#clear')
+        this.valueInput = document.querySelector ( 'input' );
+
+    }
+    postTodos () {
+        let arr = []
+        this.save.addEventListener ( 'click', () => {
+            document.querySelectorAll ( '.tuduElement' )
+                .forEach ( (el, i) => {
+                    arr.push(el.firstChild.innerHTML)
+                } )
+            localStorage.setItem ( 'arr', JSON.stringify ( arr ) )
+            alert('СОХРАНЕНО!')
+        } )
+    }
+
+    getTodos (data) {
+        this.addLi.addEventListener ( 'click', () => {
+            html1.addElement ( this.valueInput.value )
+        } )
+        fetch ( `https://jsonplaceholder.typicode.com/todos` )
+            .then ( (response) => response.json () )
+            .then ( (json) => ( json.map ( (item, index) => {
+                if ( index < data ) html1.addElement ( item.title )
+            } ) ) )
+    }
+    clearAll(){
+        this.clear.addEventListener('click', ()=> {
+            localStorage.removeItem('arr')
+            location.reload()
+        } )
+    }
+    inputEvent(){
+        this.valueInput.addEventListener('keypress', (e)=>{
+            if(e.key==='Enter') html1.addElement(this.valueInput.value)
+        })
+    }
+
+ 
+}
+const listener  = new listeners()
+export default listener
---
+++ js/Script.js
@@ -0,0 +1,62 @@
+import listener from "./Requests.js";
+class html {
+    constructor () {
+        this.classDiv = document.getElementsByClassName ( 'liClass' )[ 0 ];
+    }
+
+    init (data) {
+        if(localStorage.getItem('arr')){
+          JSON.parse(localStorage.getItem('arr')).map((el)=>{
+              this.addElement(el)
+          })
+        }else{
+            listener.getTodos(data)
+        }
+
+    }
+
+    editElement (e) {
+        e.path[ 2 ].children[ 0 ].contentEditable = 'true';
+        e.path[ 2 ].children[ 0 ].focus ();
+    }
+
+
+
+    addElement (data) {
+        let div = document.createElement ( 'div' );
+        div.className = 'tuduElement';
+        // div.style.border = 'solid'
+        div.style.margin = '20px';
+        this.classDiv.append ( div );
+        let span1 = document.createElement ( 'span' );
+        span1.innerText = data;
+        listener.valueInput.value = '';
+        div.append ( span1 );
+        let div2 = document.createElement ( 'div' );
+        div2.className = 'icons';
+        div.append ( div2 );
+        let span = document.createElement ( 'span' );
+        span.className = 'editSpan';
+        span.innerText = '✎';
+        span.onclick = (e) => this.editElement ( e );
+        div2.append ( span );
+        let span2 = document.createElement ( 'span' );
+        span2.className = 'deleteSpan';
+        span2.innerText = '🗑';
+        span2.onclick = (e) => e.target.parentNode.parentElement.remove ();
+        span2.style.margin = '20px';
+        div2.append ( span2 );
+    }
+
+
+};
+
+const html1 = new html ();
+html1.init(2)
+html1.addElement()
+listener.postTodos()
+listener.getTodos()
+listener.clearAll()
+listener.inputEvent()
+export default html1
+
